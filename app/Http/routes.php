cf2b9d892ae6b6248710f8f7bd0e1d36afceea87
David Gran <dgranh@gmail.com>
2015-04-17 16:10:34 +0200

    primeras modificaciones en la ruta home para probar conexion con github

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,9 +13,9 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
-
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+Route::get('home', ['as' => 'home', 'uses' => 'HomeController@index']);
