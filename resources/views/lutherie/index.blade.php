288ebaa2bd1272cd4b9cab235bfa3efb410c36a3
David Gran <dgranh@gmail.com>
2015-05-05 16:34:54 +0200

    Firsts changes on lutheria page index

--- resources/views/lutherie/index.blade.php
+++ resources/views/lutherie/index.blade.php
@@ -2,4 +2,33 @@
 
 @section('content')
 
+<div class="container">
+
+	<div class="row">
+		<div class="col-sm-12">
+
+			<h2>Lutheria</h2>
+			<hr>
+
+			<figure style="text-align: center; padding: 0 0 1em 0">
+				<img src="http://clementepianos.pinchaaqui.info/images/lutheria.jpg" alt="" style="width: 100%; height: auto; max-width: 578px">
+			</figure>
+
+			<p>
+			Disponemos de un taller de reparación de instrumentos de cuerda donde un luthier se dedica exclusivamente al mantenimiento y reparación de violines, violas, violonchelos y contrabajos así como de encerdar y reparar arcos.  Aquí se realizan todos los trabajos necesarios para mantener su instrumento en perfectas condiciones, todo ello con la garantía de que su instrumento siempre es tratado por un profesional.
+			</p>
+
+			<p>
+			Actualmente, colaboramos con varios luthiers europeos, tales como Stefano Trabucchi (Cremona), Riccardo Bergonzi (Cremona), Fabio Volta (Cremona), Claude Lebet (Roma), Mathias Menanteau (Roma), Jan Strick (Bruselas), y un largo etc…, con los que coordinamos junto con nuestros clientes sus visitas a nuestras tiendas, para ofrecer la posibilidad de contactar directamente con los fabricantes de sus instrumentos, y que estos los revisen periódicamente, con el fin de mantener cada instrumento en óptimas condiciones.	
+			</p>
+
+			<p>
+			También contamos con la colaboración del archetier Pierre Guillaume (Bruselas) que nos visita periódicamente para la inspección y asesoramiento de los arcos que el mismo ha fabricado, dando a nuestros clientes la posibilidad de tener un contacto directo con el archetier.	
+			</p>
+
+		</div>
+	</div> {{-- row --}}
+
+</div> {{-- container --}}
+
 @endsection
